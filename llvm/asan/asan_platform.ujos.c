51b0474de951aae3ab441cd2f2a1420e81b2cb9f
Shevtsov Aleksey <aksarra@vk.com>
2023-12-23 21:24:19 +0300

    fixed

--- llvm/asan/asan_platform.ujos.c
+++ llvm/asan/asan_platform.ujos.c
@@ -68,11 +68,11 @@ platform_abort() {
 static bool
 asan_shadow_allocator(struct UTrapframe *utf) {
     // LAB 9: Your code here
-    if (!SHADOW_ADDRESS_VALID(utf->utf_fault_va))
-        return 0;
-    
-    sys_alloc_region(0, ROUNDDOWN((void *)utf->utf_fault_va, PAGE_SIZE), PAGE_SIZE, ALLOC_ONE | PROT_R | PROT_W);
-    return 1;
+    void *va = (void *)utf->utf_fault_va;
+    if (!SHADOW_ADDRESS_VALID(va)) {
+        return 0; 
+    }
+    return sys_alloc_region(sys_getenvid(), ROUNDDOWN(va, PAGE_SIZE), SHADOW_STEP, ALLOC_ONE | PROT_RW) == 0;
 }
 #endif
 
@@ -140,7 +140,7 @@ platform_asan_init(void) {
     /* 4. Shared pages
      * HINT: Use foreach_shared_region() with asan_unpoison_shared_region() */
     // LAB 8: Your code here
-    foreach_shared_region(asan_unpoison_shared_region, 0);
+    foreach_shared_region(asan_unpoison_shared_region, NULL);
     // TODO NOTE: LAB 11 code may be here
 }
 
